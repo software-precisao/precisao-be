919643e095651a4edac5738d49fa463b4a6a7c86
Ragner Moura <44169840+ragnermoura@users.noreply.github.com>
2024-08-09 19:35:25 +0100

    Publish

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ require('dotenv').config()
 const conn = require('./data/conn')
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
-
 conn.sync().then(() => {
     server.listen(port);
     console.log('foi iniciado na porta: ' + port);
