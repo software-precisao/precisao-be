a3d40ef41ef81417c0100a586b2576e05e03aa67
andressaricardo-precisao <andressa@softwareprecisao.com>
2024-08-13 11:55:07 -0300

    fix: correção no arquivo de configuração do swagger

--- swagger.js
+++ swagger.js
@@ -7,7 +7,7 @@ const swaggerDefinition = {
     },
     servers: [
       {
-        url: 'http//localhost:3000',
+        url: 'http://localhost:3000',
         description: 'Servidor de Homologação',
       },
       {
