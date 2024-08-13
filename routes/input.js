a8360260673923cdd21566f907279ca1c43e76f5
andressaricardo-precisao <andressa@softwareprecisao.com>
2024-08-13 11:55:57 -0300

    feat: criando swagger para o input

--- routes/input.js
+++ routes/input.js
@@ -8,19 +8,139 @@ const {
   deletarInput
 } = require('../controllers/input/inputController');
 
-// Rota para criar um novo input
-router.post('/cadastrar', criarInput);
-
-// Rota para obter todos os inputs
+/**
+ * @swagger
+ * /input:
+ *   get:
+ *     summary: Obtém todos os inputs
+ *     tags: [Inputs]
+ *     responses:
+ *       200:
+ *         description: Lista de inputs retornada com sucesso
+ *       500:
+ *         description: Erro interno do servidor
+ */
 router.get('/', obterInputs);
 
-// Rota para obter um input por ID
+/**
+ * @swagger
+ * /input/{id_input}:
+ *   get:
+ *     summary: Obtém um input pelo ID
+ *     tags: [Inputs]
+ *     parameters:
+ *       - in: path
+ *         name: id_input
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID do input
+ *     responses:
+ *       200:
+ *         description: Input retornado com sucesso
+ *       404:
+ *         description: Input não encontrado
+ *       500:
+ *         description: Erro interno do servidor
+ */
 router.get('/:id_input', obterInputPorId);
 
-// Rota para atualizar um input por ID
+/**
+ * @swagger
+ * /input/cadastrar:
+ *   post:
+ *     summary: Cria um novo input
+ *     tags: [Inputs]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nome_input:
+ *                 type: string
+ *                 example: "Email"
+ *               type:
+ *                 type: string
+ *                 example: "text"
+ *               input:
+ *                 type: text
+ *                 example: "Insira o seu email"
+ *               required:
+ *                 type: integer
+ *                 example: 1
+ *     responses:
+ *       201:
+ *         description: Input criado com sucesso
+ *       500:
+ *         description: Erro interno do servidor
+ */
+router.post('/cadastrar', criarInput);
+
+/**
+ * @swagger
+ * /input/editar/{id_input}:
+ *   put:
+ *     summary: Atualiza um input pelo ID
+ *     tags: [Inputs]
+ *     parameters:
+ *       - in: path
+ *         name: id_input
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID do input
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nome_input:
+ *                 type: string
+ *                 example: "Telefone"
+ *               type:
+ *                 type: string
+ *                 example: "number"
+ *               input:
+ *                 type: text
+ *                 example: "Insira o seu telefone"
+ *               required:
+ *                 type: integer
+ *                 example: 1
+ *     responses:
+ *       200:
+ *         description: Input atualizado com sucesso
+ *       404:
+ *         description: Input não encontrado
+ *       500:
+ *         description: Erro interno do servidor
+ */
 router.put('/editar/:id_input', atualizarInput);
 
-// Rota para deletar um input por ID
+/**
+ * @swagger
+ * /input/deletar/{id_input}:
+ *   delete:
+ *     summary: Deleta um input pelo ID
+ *     tags: [Inputs]
+ *     parameters:
+ *       - in: path
+ *         name: id_input
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID do input
+ *     responses:
+ *       200:
+ *         description: Input deletado com sucesso
+ *       404:
+ *         description: Input não encontrado
+ *       500:
+ *         description: Erro interno do servidor
+ */
 router.delete('/deletar/:id_input', deletarInput);
 
 module.exports = router;
