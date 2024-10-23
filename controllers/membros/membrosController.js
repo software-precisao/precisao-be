e0f9ea113c06c2b235048adf3bea915e817634c6
andressaricardo-precisao <andressa@softwareprecisao.com>
2024-10-23 14:48:04 -0300

    feat: validacao se o id_cargo e id_funcao existem

--- controllers/membros/membrosController.js
+++ controllers/membros/membrosController.js
@@ -1,13 +1,34 @@
 const Membro = require("../../models/tb_membros");
+const Cargo = require("../../models/tb_cargo"); // Model de Cargo
+const Funcao = require("../../models/tb_funcao"); // Model de Funcao
+
+const verificarIdExistente = async (id, modelo) => {
+  const existe = await modelo.findByPk(id);
+  return existe !== null;
+};
 
 const criarMembro = async (req, res) => {
   try {
+    const { id_cargo, id_funcao } = req.body;
+
+    const cargoExistente = await verificarIdExistente(id_cargo, Cargo);
+    const funcaoExistente = await verificarIdExistente(id_funcao, Funcao);
+
+    if (!cargoExistente) {
+      return res.status(400).send({ message: "ID de cargo não existe" });
+    }
+
+    if (!funcaoExistente) {
+      return res.status(400).send({ message: "ID de função não existe" });
+    }
+
     const membro = await Membro.create(req.body);
     return res.status(201).send({ message: "Membro criado com sucesso!", membro });
   } catch (error) {
     return res.status(500).send({ error: error.message });
   }
 };
+
 
 const obterMembroPorId = async (req, res) => {
   try {
@@ -38,12 +59,24 @@ const atualizarMembro = async (req, res) => {
     if (!membro) {
       return res.status(404).send({ message: "Membro não encontrado" });
     }
+
+    const { id_cargo, id_funcao } = req.body;
+
+    if (id_cargo && !(await verificarIdExistente(id_cargo, Cargo))) {
+      return res.status(400).send({ message: "ID de cargo não existe" });
+    }
+
+    if (id_funcao && !(await verificarIdExistente(id_funcao, Funcao))) {
+      return res.status(400).send({ message: "ID de função não existe" });
+    }
+
     await membro.update(req.body);
     return res.status(200).send({ message: "Membro atualizado com sucesso!", membro });
   } catch (error) {
     return res.status(500).send({ error: error.message });
   }
 };
+
 
 const deletarMembro = async (req, res) => {
   try {
